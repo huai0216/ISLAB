5539e17af500a7f1a01419fefbfb06b1f39d0a37
huai0216 <52515300+huai0216@users.noreply.github.com>
2021-10-16 22:01:54 +0800

    Create fa.v

---
+++ ripple_carry_adder_16bits/fa.v
@@ -0,0 +1,14 @@
+
+module fa(a, b, ci, sum, cout);
+input a, b, ci;
+output  sum, cout;
+wire ha1_sum, ha2_sum, ha1_carry, ha2_carry, fsum, fcout;
+
+ha ha1(.a(a), .b(b), .sum(ha1_sum), .cout(ha1_carry));
+ha ha2(.a(ha1_sum), .b(ci), .sum(ha2_sum), .cout(ha2_carry));
+
+assign sum = ha2_sum;
+assign cout = ha1_carry | ha2_carry;
+
+
+endmodule
